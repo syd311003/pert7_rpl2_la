8b33a78310b804add32b67926c4982e4afeac6e3
sydjelek <130268705+syd311003@users.noreply.github.com>
2025-12-05 08:56:27 +0700

    File semua act

---
+++ ACT3/src/main/java/com/mycompany/pert3rpl/Pert3Rpl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3rpl;
+
+/**
+ *
+ * @author sydd
+ */
+public class Pert3Rpl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
